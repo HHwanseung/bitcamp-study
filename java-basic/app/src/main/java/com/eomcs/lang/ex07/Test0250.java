bef9b447589559a134345bcd7628feee5027f097
seunghwanLee <dkqlch@naver.com>
2021-06-21 18:00:28 +0900

    .

--- java-basic/app/src/main/java/com/eomcs/lang/ex07/Test0250.java
+++ java-basic/app/src/main/java/com/eomcs/lang/ex07/Test0250.java
@@ -4,16 +4,16 @@ public class Test0250 {
 
 	public static void hello(String...name) {
 		for(int i = 0; i < name.length; i++) {
-			System.out.printf("%s님 반갑습니다! ", name[i]);
+			System.out.printf("%s님 반갑습니다.\n", name[i]);
 		}
 	}
 	public static void main(String[] args) {
 		hello("최한빛");
-		System.out.println("-----------");
-		hello("최한빛" , "이승환");
-		System.out.println("---------------");
+		System.out.println("\n---------------");
+		hello("최한빛" ,"이승환");
+		System.out.println("\n---------------");
 		hello("최한빛", "이승환", "남승범");
-		System.out.println("---------------");
+		System.out.println("\n---------------");
 		
 		String[] arr = {"최한빛", "이승환","남승범"};
 		hello(arr);
